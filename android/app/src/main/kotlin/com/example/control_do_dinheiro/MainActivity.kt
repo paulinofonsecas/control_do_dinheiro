2c2722b94a739b68bed0ee7c864396e16f0c863d
Paulino Fonseca <paulinofonsecass@gmail.com>
2020-10-24 18:55:45 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/example/control_do_dinheiro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.control_do_dinheiro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/modules/entitys/pessoa.dart
@@ -0,0 +1,10 @@
+abstract class Pessoa {
+  final String bi;
+  final String nome;
+  final DateTime dataDeAniversario;
+  final String morada;
+  final String urlDaFoto;
+
+  Pessoa(
+      this.bi, this.nome, this.dataDeAniversario, this.morada, this.urlDaFoto);
+}
