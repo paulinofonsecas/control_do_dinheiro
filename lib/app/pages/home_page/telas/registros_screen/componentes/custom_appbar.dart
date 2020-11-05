cdfa185f3b0775bbd924430da3df519da4bdf14d
Paulino Fonseca <paulinofonsecass@gmail.com>
2020-11-06 00:16:28 +0100

    Implementando a tela de registro de venda

---
+++ lib/app/pages/home_page/telas/registros_screen/componentes/custom_appbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final bool isEmpty;
+  final List<Widget> actions;
+  final String title;
+  const CustomAppBar({
+    Key key,
+    this.isEmpty,
+    this.actions,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 10,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Spacer(),
+            Row(
+              children: actions,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/app/pages/home_page/telas/resgistros_screen.dart
+++ lib/app/pages/home_page/telas/resgistros_screen.dart
@@ -1,63 +1,25 @@
+import 'package:control_do_dinheiro/app/controllers/registros_screen.dart';
 import 'package:control_do_dinheiro/app/pages/home_page/componentes/registro_item.dart';
+import 'package:control_do_dinheiro/app/pages/home_page/telas/registros_screen/componentes/custom_appbar.dart';
 import 'package:flutter/material.dart';
 
-class RegistroScreen extends StatelessWidget {
+class RegistroScreen extends StatefulWidget {
   RegistroScreen({
     Key key,
   }) : super(key: key);
 
-  final registros = [
-    Registro(
-        nomeDoFuncionario: 'Cailoy',
-        entrada: 18000,
-        saida: 1500,
-        dateTime: DateTime.now()),
-    Registro(
-        nomeDoFuncionario: 'Mario',
-        entrada: 16500,
-        saida: 2500,
-        dateTime: DateTime.now()),
-    Registro(
-        nomeDoFuncionario: 'Zeca',
-        entrada: 17150,
-        saida: 3000,
-        dateTime: DateTime.now()),
-    Registro(
-        nomeDoFuncionario: 'Kapapelo',
-        entrada: 22300,
-        saida: 500,
-        dateTime: DateTime.now()),
-    Registro(
-        nomeDoFuncionario: 'Cailoy',
-        entrada: 24850,
-        saida: 1000,
-        dateTime: DateTime.now()),
-    Registro(
-        nomeDoFuncionario: 'Cailoy',
-        entrada: 10220,
-        saida: 2200,
-        dateTime: DateTime.now()),
-    Registro(
-        nomeDoFuncionario: 'Cailoy',
-        entrada: 7000,
-        saida: 200,
-        dateTime: DateTime.now()),
-    Registro(
-        nomeDoFuncionario: 'Mario',
-        entrada: 8350,
-        saida: 500,
-        dateTime: DateTime.now()),
-    Registro(
-        nomeDoFuncionario: 'Zeca',
-        entrada: 14200,
-        saida: 6000,
-        dateTime: DateTime.now()),
-    Registro(
-        nomeDoFuncionario: 'Kapapelo',
-        entrada: 5300,
-        saida: 8000,
-        dateTime: DateTime.now()),
-  ];
+  @override
+  _RegistroScreenState createState() => _RegistroScreenState();
+}
+
+class _RegistroScreenState extends State<RegistroScreen> {
+  RegistrosScreenController _controller;
+
+  @override
+  void initState() {
+    _controller = RegistrosScreenController(context);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,46 +28,69 @@ class RegistroScreen extends StatelessWidget {
         backgroundColor: Color(0xff282936),
         body: Column(
           children: [
-            _buildCustomAppBar(),
-            SizedBox(height: 25),
-            Expanded(
-              child: Container(
-                child: registros.isEmpty
-                    ? Center(
-                        child: Text(
-                          'Sem registros',
-                          style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.white,
-                          ),
-                        ),
-                      )
-                    : ListView.builder(
-                        itemCount: registros.length,
-                        itemBuilder: (_, index) =>
-                            RegistroItem(registro: registros[index]),
-                      ),
-              ),
+            FutureBuilder<List<Registro>>(
+              future: _controller.registros,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  var registros = snapshot.data;
+                  return Column(
+                    children: [
+                      _buildCustomAppBar(registros: registros),
+                      SizedBox(height: 25),
+                      buildList(registroList: registros),
+                    ],
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
             ),
           ],
         ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Color(0xff41d1e2),
-          child: Icon(
-            Icons.add,
-            color: Color(0xff282936)
+        floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: Color(0xFF3CABD6),
+          icon: Icon(Icons.add, color: Color(0xff282936)),
+          label: Text(
+            'Rigistrar venda',
+            style: TextStyle(
+              fontSize: 18,
+            ),
           ),
-          onPressed: () {},
+          onPressed: _controller.registrarVenda,
         ),
       ),
     );
   }
 
-  bool get isEmpty => registros.isEmpty;
+  Expanded buildList({List<Registro> registroList}) {
+    var registros = registroList;
+    return Expanded(
+      child: Container(
+        child: registros.isEmpty
+            ? Center(
+                child: Text(
+                  'Sem registros',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            : ListView.builder(
+                itemCount: registros.length,
+                itemBuilder: (_, index) =>
+                    RegistroItem(registro: registros[index]),
+              ),
+      ),
+    );
+  }
 
-  Widget _buildCustomAppBar() {
+  Widget _buildCustomAppBar({List<Registro> registros}) {
+    var isEmpty = registros.isEmpty;
     return CustomAppBar(
-      isEmpty: registros.isEmpty,
+      isEmpty: isEmpty,
       title: 'Registros',
       actions: [
         IconButton(
@@ -125,48 +110,6 @@ class RegistroScreen extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class CustomAppBar extends StatelessWidget {
-  final bool isEmpty;
-  final List<Widget> actions;
-  final String title;
-  const CustomAppBar({
-    Key key,
-    this.isEmpty,
-    this.actions,
-    this.title,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topLeft,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20.0,
-          vertical: 10,
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              title,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Spacer(),
-            Row(
-              children: actions,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
